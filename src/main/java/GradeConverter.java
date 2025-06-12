23f2c2fd4710310a277a4cd6af4a6818497aae53
1ThaoVy <huathithaovydhi@gmail.com>
2025-06-12 09:30:14 +0700

    sua loi ne

--- src/main/java/GradeConverter.java
+++ src/main/java/GradeConverter.java
@@ -4,9 +4,15 @@ public class GradeConverter {
     {
         if(score <= 100 && score >= 90) {      return "A";
         } else if(score <= 89 && score >= 80) {     return "B";
+<<<<<<< HEAD
         } else if(score <= 79 && score > 70){
             return "C";
         } else if(score <= 69 && score > 60) {
+=======
+        } else if(score <= 79 && score >= 70){
+            return "C";
+        } else if(score <= 69 && score >= 60) {
+>>>>>>> a0581db (sua loi)
             return "D";
         } else if(score <= 59 && score > 0) {
             return "E";
